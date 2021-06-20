5a4d67a7c346dd3a5d486fca40a8267d232171d0
priyankang <priyankak18@navgurukul.org>
2021-06-20 23:22:33 +0530

    changing background color

---
+++ page.js
@@ -0,0 +1,6 @@
+function func() {
+    var color = document.getElementById("colorInput").value;
+    document. body.style.backgroundColor = color;
+    document.getElementById("in_btn").value = color;
+
+  }
